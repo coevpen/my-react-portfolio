62030f7102887b6903d2fe9ca0d24877bfea9bd7
coevpen <koinuevins@gmail.com>
2022-03-21 15:49:05 -0500

    initial component create and install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from './Components/Nav';
+import About from './Components/About';
+import Portfolio from './Components/Portfolio';
+import Resume from './Components/Resume';
+import ContactForm from './Components/Contact';
+
+// the <main> will contain the rendered pages
+// the <nav> will have the links
+// the <header> will have the hero and nav
+// the footer will hold the linkedIn and GitHub links
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Nav />
       </header>
+
+      <main>
+        
+      </main>
+
+      <footer>
+
+      </footer>
     </div>
   );
 }
