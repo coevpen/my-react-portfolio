6295bb5937f5179ae9d9e44624038961867bb7c7
coevpen <koinuevins@gmail.com>
2022-03-25 22:34:27 -0500

    update css, add portfolio page and individual cards

--- src/Components/Page/PageContent/Portfolio/Project/index.js
+++ src/Components/Page/PageContent/Portfolio/Project/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-function Project(){
+
+function Project(props){
 
 
     return (
-        <div>
-
-        </div>
+        <Row>
+            {props.projects.map((project) => (
+                <Col md={4}>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                            <Card.Title>{project.title}</Card.Title>
+                            <Card.Text>
+                                Tech/Languages Used: {project.languages}
+                            </Card.Text>
+                            <Card.Text>
+                                {project.description}
+                            </Card.Text>
+                            <Card.Text>
+                                <a href={project.deployed} target="_blank" rel="noreferrer">Deployed Site</a>
+                            </Card.Text>
+                            <Card.Text>
+                                <a href={project.repo} target="_blank" rel="noreferrer">Repo location</a>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            ))}
+        </Row>
     );
 }
 
--- src/Components/Page/PageContent/Portfolio/index.js
+++ src/Components/Page/PageContent/Portfolio/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Project from './Project';
 
+// creates an array of the projects to be displayed
 const projects = [
     {
         id: 0,
         title: "Pick of the Day",
-        languages: "java 8",
+        languages: "Java 8, JavaFX SceneBuilder 2.0",
         image:"",
         description: "This application keeps track of what the user is reading/watching and keeps a record of it in a file. The user can choose at random what to watch or read and can mark them as completed, removing them from the file or mark where they left off.",
         deployed: "https://drive.google.com/file/d/1ZEqfNSFtg5zZYfOKlmQ5l_TEVub7hwS5/view",
@@ -14,7 +15,7 @@ const projects = [
     {
         id: 1,
         title: "My Night Sky",
-        languages: "html, css, bulma, javascript",
+        languages: "HTML, CSS, Bulma, JavaScript",
         image:"",
         description: "A website for astronomy lovers to see where the ISS is according to their location and what phase of the moon it currently is.",
         deployed: "https://coevpen.github.io/my-night-sky/",
@@ -23,7 +24,7 @@ const projects = [
     {
         id: 2,
         title: "We Rate Pets",
-        languages: "nodejs, javascript, mysql, handlebars, sequelize, express",
+        languages: "Node.js, JavaScript, MySQL, Express Handlebars, Sequelize, Express",
         image:"",
         description: "We Rate Pets is a social media site where you can upload photos of your pets and view others. You can leave comments on the photo posts and rate the pet! The idea was based on We Rate Dogs but broadens it to all animals/pets. You can browse the site or create an account to share and comment.",
         deployed: "https://infinite-sea-93313.herokuapp.com/",
@@ -32,7 +33,7 @@ const projects = [
     {
         id: 3,
         title: "Weather Forecast",
-        languages: "html, css, javascript, openweather api",
+        languages: "HTML, CSS, JavaScript, OpenWeather API",
         image:"",
         description: "A site that provides the 5-day weather forecast based on the city name searched. It saves search history as buttons for easy searches.",
         deployed: "https://coevpen.github.io/weather-forecast/",
@@ -41,7 +42,7 @@ const projects = [
     {
         id: 4,
         title: "JavaScript Timed Quiz",
-        languages: "html, css, javascript",
+        languages: "HTML, CSS, JavaScript",
         image:"",
         description: "A timed quiz about javascript. It takes time off the clock for every wrong answer and provides an ending high score.",
         deployed: "https://coevpen.github.io/javascript-time-quiz/",
@@ -50,7 +51,7 @@ const projects = [
     {
         id: 5,
         title: "My Portfolio",
-        languages: "html, css",
+        languages: "HTML, CSS",
         image:"",
         description: "This was the first website I made. It's my portolio with some beginning projects.",
         deployed: "https://coevpen.github.io/myportfolio/",
@@ -59,7 +60,7 @@ const projects = [
 ];
 
 
-
+// sends the array as props to the project file and is rendered
 function Portfolio(){
     return (
         <div>
