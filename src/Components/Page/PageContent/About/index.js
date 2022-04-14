e35cfe76989a9934b18ace4cf314558bae16e01f
coevpen <koinuevins@gmail.com>
2022-04-13 23:22:50 -0500

    fixed conflict

--- src/Components/Page/PageContent/About/index.js
+++ src/Components/Page/PageContent/About/index.js
@@ -6,7 +6,7 @@ function About(){
             <h2 className="about-me-h2">About Me:</h2>
             <p className="about-me">I'm a Software Engineer with a certificate in Full Stack Web Development and pursuing a Bachelor's degree in Computer Science. 
             I leverage my customer service and logistics background to bridge the gap between backend and an intuitive user interface. I'm known by my peers as
-            being an essential member to the team and I'm excited to continue learning and growing as programmer. 
+            being an essential member to the team and I'm excited to continue learning and growing as a programmer. 
             </p>
             <p className="about-me">
             As a natural leader, I've helmed project planning and kept communication open and ongoing between team members on multiple projects such of Pick of the Day and We Rate Pets.
